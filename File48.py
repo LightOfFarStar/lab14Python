d517c733f6bdecdaba9fb5dbef334c5c1acf375c
LightOfFarStar <123jinga@gmail.com>
2026-02-20 10:21:06 +0500

    Исправления

--- File48.py
+++ File48.py
@@ -1,4 +1,5 @@
 import random
+import pickle
 
 filename_a = "SA.txt"
 filename_b = "SB.txt"
@@ -11,12 +12,12 @@ numbers_a = [random.randint(1, 100) for _ in range(number)]
 numbers_b = [random.randint(1, 100) for _ in range(number)]
 numbers_c = [random.randint(1, 100) for _ in range(number)]
 
-with open(filename_a, 'w') as fa:
-    fa.write(' '.join(map(str, numbers_a)))
-with open(filename_b, 'w') as fb:
-    fb.write(' '.join(map(str, numbers_b)))
-with open(filename_c, 'w') as fc:
-    fc.write(' '.join(map(str, numbers_c)))
+with open(filename_a, 'wb') as fa:
+    pickle.dump((' '.join(map(str, numbers_a))), fa)
+with open(filename_b, 'wb') as fb:
+    pickle.dump((' '.join(map(str, numbers_b))), fb)
+with open(filename_c, 'wb') as fc:
+    pickle.dump((' '.join(map(str, numbers_c))), fc)
 
 print("Созданы исходные файлы:")
 print(f"{filename_a}: {' '.join(map(str, numbers_a))}")
@@ -24,10 +25,13 @@ print(f"{filename_b}: {' '.join(map(str, numbers_b))}")
 print(f"{filename_c}: {' '.join(map(str, numbers_c))}")
 
 try:
-    with open(filename_a, 'r') as fa, open(filename_b, 'r') as fb, open(filename_c, 'r') as fc:
-        numbers_a = [int(x) for x in fa.read().split()]
-        numbers_b = [int(x) for x in fb.read().split()]
-        numbers_c = [int(x) for x in fc.read().split()]
+    with open(filename_a, 'rb') as fa, open(filename_b, 'rb') as fb, open(filename_c, 'rb') as fc:
+        fa_exp = pickle.load(fa)
+        fb_exp = pickle.load(fb)
+        fc_exp = pickle.load(fc)
+        numbers_a = [int(x) for x in fa_exp.split()]
+        numbers_b = [int(x) for x in fb_exp.split()]
+        numbers_c = [int(x) for x in fc_exp.split()]
     
     if len(numbers_a) != len(numbers_b) or len(numbers_a) != len(numbers_c):
         print("Предупреждение: файлы имеют разный размер!")
@@ -43,25 +47,25 @@ try:
         result.append(b)
         result.append(c)
     
-    with open(filename_d, 'w') as fd:
-        fd.write(' '.join(map(str, result)))
+    with open(filename_d, 'wb') as fd:
+        pickle.dump((' '.join(map(str, result))), fd)
     
-    print(f"Файл {filename_d} успешно создан!")
+    print(f"Файл {filename_d} успешно создан")
     print(f"Обработано {len(numbers_a)} элементов из каждого файла.")
     print(f"Всего записано {len(result)} чисел.")
-    with open(filename_d, 'r') as fd:
-        content = fd.read()
+    with open(filename_d, 'rb') as fd:
+        content = pickle.load(fd)
         print(f"Содержимое файла {filename_d}:")
         print(content)
-except FileNotFoundError as e:
-    print(f"Ошибка: не найден файл - {e.filename}")
-except ValueError as e:
+except FileNotFoundError as err:
+    print(f"Ошибка: не найден файл - {err.filename}")
+except ValueError as err:
     print("Ошибка: в файлах содержатся не только целые числа")
     print("Убедитесь, что файлы содержат только целые числа, разделенные пробелами")
-except Exception as e:
-    print(f"Произошла непредвиденная ошибка: {e}")
+except Exception as err:
+    print(f"Произошла непредвиденная ошибка: {err}")
 
 '''File48. Даны три файла целых чисел одинакового размера с именами
-SA, SB, SCистрокаSD.Создать новый файл с именем SD, в котором чередовались бы элементы
+SA, SB, SC и строка SD.Создать новый файл с именем SD, в котором чередовались бы элементы
 исходных файлов с одним и тем же номером:
 A1, B1, C1, A2, B2, C2, … .'''
--- file1.py
+++ file1.py
@@ -5,7 +5,8 @@ try:
     print("True")
 except FileNotFoundError:
     print('False')
-
+except IsADirectoryError:
+    print('False')
 '''File1. Дана строка S. Если S является допустимым именем файла,
 то создать пустойфайлсэтимименем и вывести True.
 Если файл с именем S создать нельзя, то вывести False.'''
